a40a2952795d3ff461c2a2387d4cf7afefb077fe
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2019-12-05 12:16:17 +0000

    stop copying into "res/res"

--- copy_files_to_kiwix_android.sh
+++ copy_files_to_kiwix_android.sh
@@ -6,7 +6,7 @@ for f in *; do # for all files in current directory
         if [ -f "$f/info.json" ] && [ -d "$f/res" ]; then #that has a json file and icon set
             mkdir -p "kiwix-android/custom/src/$f/res" #make directory
             cp -v "$f/info.json" "kiwix-android/custom/src/$f/info.json" # copy json over
-            cp -vr "$f/res" "kiwix-android/custom/src/$f/res" #copy icons over
+            cp -vr "$f/res" "kiwix-android/custom/src/$f" #copy icons over
         fi
     fi
 done
